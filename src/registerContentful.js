b320619e759516ce4edb0377ec346055c82887b6
Stephanie Tassone <stephanie.tassone@gmail.com>
2018-02-06 13:52:47 +1100

    add configs

--- src/registerContentful.js
+++ src/registerContentful.js
@@ -1,9 +1,8 @@
 import { createClient } from 'contentful';
 // singelton pattern
 const client = createClient({
-  space: '5v32339pogmo',
-  accessToken:
-    'e0b47ac40dfa7e8fb22f68a00efbc35f082c43e82ea4bec68137d71e890e89cf'
+  space: 'CONTENTFUL_SPACE',
+  accessToken: 'CONTENTFUL_ACCESSTOKEN',
 });
 
 export default client;
